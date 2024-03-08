42f0d682c6359516fd2272d27ad9ab3fc615335c
Daniel Gibson <danielgibson.dev@gmail.com>
2024-03-08 15:28:08 +0000

    add form styling, yup validations, and basic formik

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,8 +2,10 @@ import { SearchForm } from './SearchForm/SearchForm'
 
 export const MainPage: React.FC = () => {
   return (
-    <div>
-      <div>MainPage</div>
+    <div className="flex flex-col items-center">
+      <h1 className="my-8 max-w-3xl text-center text-3xl font-extrabold leading-none tracking-tight text-gray-900">
+        List GitHub Public Repositories from a username or organisation name
+      </h1>
       <SearchForm />
     </div>
   )
--- src/pages/MainPage/SearchForm/SearchForm.tsx
+++ src/pages/MainPage/SearchForm/SearchForm.tsx
@@ -1,37 +1,63 @@
-import { ChangeEvent, useState, FormEvent } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
-interface FormData {
+interface FormValues {
   searchTerm: string
 }
 
+const SearchFormSchema = Yup.object().shape({
+  searchTerm: Yup.string()
+    .min(1, 'Too short!')
+    .max(100, 'Too long!')
+    .required('Required'),
+})
+
 export const SearchForm: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({
-    searchTerm: '',
+  const initialValues: FormValues = { searchTerm: '' }
+
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: initialValues,
+    validationSchema: SearchFormSchema,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (values) => {
+      console.log(values)
+    },
   })
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    })
-  }
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter GitHub Username
-        <input
-          type="text"
-          name="searchTerm"
-          value={formData.searchTerm}
-          onChange={handleInputChange}
-        />
+    <form onSubmit={handleSubmit} className="flex flex-col items-center">
+      <label
+        htmlFor="searchTerm"
+        className="mb-2 block text-md font-medium leading-6 text-gray-900"
+      >
+        Enter a GitHub Username or Organisation name
       </label>
-      <button type="submit">Search</button>
+      <div className="flex gap-4 justify-center items-center">
+        <div>
+          <input
+            id="searchTerm"
+            type="text"
+            name="searchTerm"
+            value={values.searchTerm}
+            onChange={handleChange}
+            placeholder="e.g. Netflix"
+            className="block border-1 rounded-md border-0 py-1.5 px-4 placeholder:text-gray-400 ring-1 ring-inset ring-gray-300"
+          />
+          {errors.searchTerm ? (
+            <div className="text-s text-red-600 absolute">
+              {errors.searchTerm}
+            </div>
+          ) : null}
+        </div>
+
+        <button
+          type="submit"
+          className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none"
+        >
+          Find Repositories
+        </button>
+      </div>
     </form>
   )
 }
